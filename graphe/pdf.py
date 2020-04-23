3a9a155224962ff88f130692e27f6d713f71f1bf
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2020-04-23 02:39:41 +0100

    Added more to PDF module

--- graphe/pdf.py
+++ graphe/pdf.py
@@ -1,4 +1,19 @@
+from datetime import datetime
 
+
+class PDFName(object):
+    def __init__(self, value):
+        self.value = value
+
+
+class PDFString(object):
+    def __init__(self, value):
+        self.value = value
+
+
+class PDFDate(object):
+    def __init__(self, value):
+        self.value = value
 
 
 class PDFDocument (object):
@@ -6,15 +21,67 @@ class PDFDocument (object):
         self.header = PDFHeader()
         self.crossReferenceTable = PDFCrossReferenceTable()
 
+
 class PDFHeader(object):
-    def __init__(self, version = "1.1"):
+    def __init__(self, version="1.1"):
         self.version = version
+
 
 class PDFCrossReferenceTable(object):
     def __init__(self):
         pass
 
+
 class PDFIndirectObject(object):
     def __init__(self):
         self.id = 0
         self.generation = 0
+
+    def getDictionary(self):
+        return {}
+
+
+class PDFPageObject(PDFIndirectObject):
+    def __init__(self):
+        self.parent = None
+        self.mediaBoxLeft = 0
+        self.mediaBoxTop = 0
+        self.mediaBoxWidth = 0
+        self.mediaBoxHeight = 0
+        self.rotation = 0
+        self.contents = []
+        self.resources = {}
+
+    def getDictionary(self):
+        return [
+            [PDFName("Type"), PDFName("Page")]
+        ]
+
+
+class PDFDocumentInformationObject(PDFIndirectObject):
+    def __init__(self):
+        self.title = ""
+        self.author = ""
+
+    def getDictionary(self):
+        return [
+            [PDFName("Title"), PDFString(self.title)],
+            [PDFName("Author"), PDFString(self.author)],
+            [PDFName("Producer"), PDFString("Graphe")],
+            [PDFName("ModDate"), PDFDate(datetime.now())],
+            [PDFName("CreationDate"),  PDFDate(datetime.now())],
+        ]
+
+
+class PDFWriter(object):
+    def __init__(self):
+        pass
+
+    def writeDocument(self, filePath, pdfDocument):
+        with open(filePath, "w") as fileObject:
+            self._writeHeader(fileObject, pdfDocument.header)
+
+            fileObject.write("%%%%EOF")
+
+    def _writeHeader(self, fileObject, pdfHeader):
+        fileObject.write("%%PDF-{0}".format(pdfHeader.version))
