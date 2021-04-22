6036401b2f7f6651099e1b6ea8685d4e4a2d153a
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2021-04-22 23:50:17 +0100

    Added more to XPath module

--- graph/xpath.py
+++ graph/xpath.py
@@ -1,23 +1,94 @@
 
 
+class XPathExpression(object):
+    def __init__(self):
+        self.selectors = []
 
-class Selector(object):
-    pass 
 
-class ElementNameSelector(Selector):
+class XPathSelector(object):
+    pass
+
+
+class ElementNameSelector(XPathSelector):
     def __init__(self, elementName):
         super(ElementNameSelector, self).__init__()
 
-        self.elementName = elementName 
+        self.elementName = elementName
 
-class SubelementsSelector(Selector):
+
+class SubelementsSelector(XPathSelector):
     def __init__(self):
         super(SubelementsSelector, self).__init__()
 
-class RootElementSelector(Selector):
+
+class RootElementSelector(XPathSelector):
     def __init__(self):
         super(RootElementSelector, self).__init__()
 
-class SuperelementSelector(Selector):
+
+class SuperelementSelector(XPathSelector):
     def __init__(self):
         super(SuperelementSelector, self).__init__()
+
+
+class Marker(object):
+    def __init__(self):
+        self.position = 0
+
+    @property
+    def p(self):
+        return self.position
+
+    @p.setter
+    def p(self, value):
+        self.position = value
+
+    def copy(self):
+        m = Marker()
+
+        m.position = self.position
+
+        return m
+
+
+def cut(text, start, length=1):
+    a = start
+    b = start + length
+
+    return text[a:b]
+
+
+class XPathParsingError(Exception):
+    def __init__(self, message):
+        super(XPathParsingError, self).__init__(message)
+
+
+class XPathParser(object):
+    _elementNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+    _attributeNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+
+    def _expect(self, text, inputText, marker):
+        c = cut(inputText, marker.p, len(text))
+
+        if c == text:
+            marker.p += len(c)
+            return True
+        else:
+            return False
+
+    def parseXPath(self, xPath):
+        marker = Marker()
+
+        expression = XPathExpression()
+
+        while marker.p < len(xPath):
+
+            if cut(xPath, marker.p, 2) == "//":
+                pass
+
+            if cut(xPath, marker.p, 1) == "/":
+                expression.selectors.append(SubelementsSelector())
+                marker.p += 1
+                continue
+
+        return expression
